f5c1df4c41b3c79c1a2a1c5e270eba0fa85a6c4a
assafmo <assaf.morami@gmail.com>
2019-03-13 11:09:01 +0200

    nba.com: follow redirects

--- nba_com_recaps.sh
+++ nba_com_recaps.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-nba_date=$( \
-curl 'https://data.nba.net/10s/prod/v3/today.json' --compressed -s | \
-jq .links.anchorDate \
+nba_date=$(
+    curl 'https://data.nba.net/10s/prod/v3/today.json' -L --compressed -s |
+        jq .links.anchorDate
 )
 
-num_of_games=$( \
-curl 'https://data.nba.net/prod/v2/calendar.json' --compressed -s | \
-jq ".[$nba_date]" \
+num_of_games=$(
+    curl 'https://data.nba.net/prod/v2/calendar.json' -L --compressed -s |
+        jq ".[$nba_date]"
 )
 
-xmls=$( \
-curl 'http://www.nba.com/video/gamerecaps' -s --compressed | \
-grep -Po 'collection_uuid":".+?"' | \
-awk -F '"' '{print $3}' | \
-xargs -I {} -n 1 curl -s --compressed https://api.nba.net/0/league/collection/{} | \
-jq . | \
-grep contentXml | \
-awk -F '"' '{print "https://www.nba.com"$4}' | \
-head -"$num_of_games" \
+xmls=$(
+    curl 'http://www.nba.com/video/gamerecaps' -L -s --compressed |
+        grep -Po 'collection_uuid":".+?"' |
+        awk -F '"' '{print $3}' |
+        xargs -I {} -n 1 curl -L -s --compressed https://api.nba.net/0/league/collection/{} |
+        jq . |
+        grep contentXml |
+        awk -F '"' '{print "https://www.nba.com"$4}' |
+        head -"$num_of_games"
 )
 
-echo "$xmls" | \
-xargs -n 1 curl -s --compressed | \
-grep -Po 'https://ssl.cdn.turner.com/nba/big/video/.+?\.mp4' | \
-grep -v '["<>]' | \
-grep -F 'x1080_' | \
-sort -u
+echo "$xmls" |
+    xargs -n 1 curl -L -s --compressed |
+    grep -Po 'https://ssl.cdn.turner.com/nba/big/video/.+?\.mp4' |
+    grep -v '["<>]' |
+    grep -F 'x1080_' |
+    sort -u
